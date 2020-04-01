6913f9d3bb65520b988344487a6a293c03f78a52
Alen <alechkr@gmail.com>
2020-04-01 12:38:00 +0200

    Created arrow functions and added comments

--- js/app.js
+++ js/app.js
@@ -18,94 +18,68 @@
  * 
 */
 
+const section = document.querySelectorAll('section');
 
 /**
  * End Global Variables
- * Start Helper Functions
+ */ 
+
+ /** Begin Main Functions
  * 
 */
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
-/**
- * Purpose: Adds sections dynamically to the navigation bar
- * 
- * Create navbarItems as anchors from sections with the method createElement()
- * Retrieve the id of each section with value.id and store it into the variable id
- * Add the section id to the anchor tag via setAttribute() method
- * Add navbarItems to the navbar__list node with the method appendChild()
- * Use dataset for retrieving the data attributes of data-nav of each section with dataset.nav and add it to the navbarItems with innerHTML
- * Usage for navigation menu: createElement(), setAttribute(), appendChild(), innerHTML, dataset for retrieving data attributes
+/** Create navbarItems as anchors from sections with the method createElement()
+ Retrieve the id of each section with value.id and store it into the variable id
+ Add the section id to the anchor tag via setAttribute() method
+ Add navbarItems to the navbar__list node with the method appendChild()
+ Use dataset for retrieving the data attributes of data-nav of each section with dataset.nav and add it to the navbarItems with innerHTML
  */
-const section = document.querySelectorAll('section');
 
-section.forEach((value) => {    
+let createNavbar = (value) => {
     const navbarItems = document.createElement('a');
-    navbarItems.classList.add('menu__link')
-    let id = "#" + value.id;
-    navbarItems.setAttribute('href', id);
-    document.querySelector('#navbar__list').appendChild(navbarItems).innerHTML = value.dataset.nav;
+        navbarItems.classList.add('menu__link')
+        let id = "#" + value.id;
+        navbarItems.setAttribute('href', id);
+        document.querySelector('#navbar__list').appendChild(navbarItems).innerHTML = value.dataset.nav;
+        console.log(value.dataset.nav)
+};
 
-    console.log(value.dataset.nav)
-})
+section.forEach(createNavbar);
 
-/**
- * Purpose: Highlight dynamically sections
- * 
- * Listen for the scroll event initiated by the user with the scroll event listener
+/** Listen for the scroll event initiated by the user with the scroll event listener
  * Retrieve the data for the y and bottom coordinates of each section with the method .getBoundingClientRect() 
  * Calculate the actual height of each section with the formula height = coordinates.bottom - coordinates.y
  * If coordinates of a section reach an upper or lower viewport area then execute the classList method and add/remove the class 'your-active-class'
  * console.log is included to monitor the coordinates of each section
- * Usage for viewport functionality: getBoundingClientRect(),y, bottom, window.innerHeight
  */
 
-window.addEventListener('scroll', () => {
-    section.forEach ((value, index) => {
+let focusSection = (index) => {
         
-        let coordinates = document.querySelectorAll('section')[index].getBoundingClientRect();
-        console.log(section[index].id + " " + coordinates.y);
-        console.log(section[index].id + " " + coordinates.bottom);
-        console.log(window.innerHeight);
-        console.log(coordinates.bottom - coordinates.y);  
-        
-        let height = coordinates.bottom - coordinates.y
+    let coordinates = document.querySelectorAll('section')[index].getBoundingClientRect();
+    let height = coordinates.bottom - coordinates.y
 
-        if (coordinates.y > -(height/4) && coordinates.bottom < window.innerHeight + (height/4)) {
-            document.querySelectorAll('section')[index].classList.add('your-active-class');
-        } else {
-            document.querySelectorAll('section')[index].classList.remove('your-active-class')
-        }
-    }); 
-    
+    if (coordinates.y > -(height/4) && coordinates.bottom < window.innerHeight + (height/4)) {
+        document.querySelectorAll('section')[index].classList.add('your-active-class');
+    } else {
+        document.querySelectorAll('section')[index].classList.remove('your-active-class')
+    }
+};
+
+/**
+ * End Main Functions
+ */ 
+ 
+ /** Begin Events
+ * 
+*/
+
+window.addEventListener('scroll', () => {
+    section.forEach (focusSection); 
 });
+
+/** End Events
+ * 
+*/
 
 
 
